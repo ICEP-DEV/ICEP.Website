2cb6d7b7c26bf79c9bb2f59c73ba3bbdac02ef17
accuballot <joel.buti97@gmail.com>
2025-06-30 16:38:03 +0200

    initial commit

--- forms/icep_form/assets/js/icepcontent.js
+++ forms/icep_form/assets/js/icepcontent.js
@@ -290,7 +290,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Prepare the FormData object to upload the file
                 const formData = new FormData();
                 formData.append('file', file);
-                formData.append('folder', CVs);
+                formData.append('folder', 'CVs');
 
                 // Send the file to the server
                 fetch(BASE_API + 'api/upload', {
